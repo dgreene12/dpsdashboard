3a0d041f469685e64ec817c7736cc85ede48f0e2
dgreene12 <acg67@duke.edu>
2024-01-16 16:13:02 -0500

    added statdata.js

---
+++ Webpages/statdata.js
@@ -0,0 +1,45 @@
+var statdata = [
+    //Elementary Schools: 
+    {
+        name: 'Racial Demographics',
+        url: 'https://raw.githubusercontent.com/dgreene12/dpsdashboard/b6ddf40cf546acb04927f3e2d9a55ff057f8c3c3/CSV%20Data%20School%20Stats/ES_all%20race%202023.csv',
+    }, 
+    {
+        name: '% People of Color',
+        url: 'https://raw.githubusercontent.com/dgreene12/dpsdashboard/b6ddf40cf546acb04927f3e2d9a55ff057f8c3c3/CSV%20Data%20School%20Stats/ES_poc_per_school_23.csv',
+    }, 
+    //High Schools:
+    {
+        name: 'Racial Demographics',
+        url: 'https://raw.githubusercontent.com/dgreene12/dpsdashboard/b6ddf40cf546acb04927f3e2d9a55ff057f8c3c3/CSV%20Data%20School%20Stats/HS_all%20race%202023.csv',
+    }, 
+    {
+        name: '% People of Color',
+        url: 'https://raw.githubusercontent.com/dgreene12/dpsdashboard/b6ddf40cf546acb04927f3e2d9a55ff057f8c3c3/CSV%20Data%20School%20Stats/HS_poc_per_school_23.csv',
+    }, 
+    {
+        name: '% People of Color, School v. School Zone',
+        url: 'https://raw.githubusercontent.com/dgreene12/dpsdashboard/b6ddf40cf546acb04927f3e2d9a55ff057f8c3c3/CSV%20Data%20School%20Stats/HS_racecomp_23.csv',
+    }, 
+    {
+        name: 'All Stats',
+        url: 'https://raw.githubusercontent.com/dgreene12/dpsdashboard/b6ddf40cf546acb04927f3e2d9a55ff057f8c3c3/CSV%20Data%20School%20Stats/HS_stats_23.csv',
+    }, 
+    //Middle Schools:
+    {
+        name: 'Racial Demographics',
+        url: 'https://raw.githubusercontent.com/dgreene12/dpsdashboard/b6ddf40cf546acb04927f3e2d9a55ff057f8c3c3/CSV%20Data%20School%20Stats/MS_all%20race%202023.csv',
+    }, 
+    {
+        name: '% People of Color',
+        url: 'https://raw.githubusercontent.com/dgreene12/dpsdashboard/b6ddf40cf546acb04927f3e2d9a55ff057f8c3c3/CSV%20Data%20School%20Stats/MS_poc_per_school_23.csv',
+    }, 
+    {
+        name: '% People of Color, School v. School Zone',
+        url: 'https://raw.githubusercontent.com/dgreene12/dpsdashboard/b6ddf40cf546acb04927f3e2d9a55ff057f8c3c3/CSV%20Data%20School%20Stats/MS_racecomp_23.csv',
+    }, 
+    {
+        name: 'All Stats',
+        url: 'https://raw.githubusercontent.com/dgreene12/dpsdashboard/b6ddf40cf546acb04927f3e2d9a55ff057f8c3c3/CSV%20Data%20School%20Stats/MS_stats_23.csv',
+    },
+]
